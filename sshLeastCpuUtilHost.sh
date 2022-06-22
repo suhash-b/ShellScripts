3757b27d36daff98a018e566cbf74afb69580654
suhash001 <105347871+suhash001@users.noreply.github.com>
2022-06-23 04:41:59 +0530

    Create sshLeastCpuUtilHost.sh

---
+++ sshLeastCpuUtilHost.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+clear
+
+# Username to connect via ssh
+USER= <UserName>
+
+# Destination path/filename to save results to
+DEST=<Outputfile>
+
+# source list of host IPs to read from
+FILE=<hostIPs>
+
+# source output of the instances for a loadbalancer
+ELB_FILE=ELB_File.out
+
+# Returns a list of instances connected to the ELB.
+aws elb describe-instance-health --load-balancer-name <ELB name> > $ELB_FILE
+<Code to extract the instance IDs>
+
+# Returns among other things the IP address of the instance.
+aws ec2 describe-instances <instance id>
+aws ec2 describe-instances | grep PublicIpAddress | grep -o -P "\d+\.\d+\.\d+\.\d+" | grep -v '^10\.'
+
+# Iterate through line items in FILE and
+# execute ssh, if we connected successfully
+# run proc/loadavg to find cpu load
+# write it to DEST path/file
+# if we don't connect successfully, write the hostname
+# and "unable to connect to host" error to DEST path/file
+
+for i in `cat $FILE`; do
+  echo -n ".";
+  CHK=`ssh -q -T -o ConnectTimeout=10 -o ConnectionAttempts=1 username@$i "echo success"`;
+  if [ "success" = $CHK ] >/dev/null 2>&1
+  then
+    `ssh -q -T -o ConnectTimeout=10 -o ConnectionAttempts=1 username@$i "\
+        printf "$i    ";
+        echo "`cat /proc/loadavg | awk '{print $3}'`";" >> ${DEST}`;
+  else
+    printf "${i}\tUnable to connect to host\n" >> ${DEST};
+  fi
+done
+
+# All line items have been gone through,
+# show done, and exit out
+echo ""
+echo "Done!"
+echo "Check the list 'checkssh_failure' for errors."
+exit 0
